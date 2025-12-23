a6c3932225bac62fa46e3635634fbc1be518dcd7
Danielo343 <jdaniel.vb08@gmail.com>
2025-12-23 13:44:17 -0600

    fix: agregar https a la api

--- Servicios Medicos/src/services/api.js
+++ Servicios Medicos/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'cvtalaprueba-production.up.railway.app/api';
+const API_BASE_URL = 'https://cvtalaprueba-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
